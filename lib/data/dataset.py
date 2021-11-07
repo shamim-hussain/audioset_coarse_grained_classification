b52c077e532bea0b3d61e210edb966feb9bd63c9
Md Shamim Hussain <snirjhar@gmail.com>
2021-11-07 15:16:58 -0500

    excluded ytid and window by default

--- lib/data/dataset.py
+++ lib/data/dataset.py
@@ -7,11 +7,15 @@ from tqdm import tqdm
 
 
 class Dataset:
-    def __init__(self, annotations_path, dataset_path, ytids_path,
-                 ytid_keys = None,
-                 class_associations=(('speech',0),('music',1),('noise',2)),
-                 transform_fn = None,
-                 load_data=True):
+    def __init__(self, 
+                 annotations_path, 
+                 dataset_path, 
+                 ytids_path,
+                 ytid_keys          =  None,
+                 class_associations = (('speech',0),('music',1),('noise',2)),
+                 transform_fn       =  None,
+                 load_data          = True,
+                 include_ytid       = False):
         self.annotations = pd.read_csv(annotations_path).set_index('ytid')
         self.dataset_path = dataset_path
         with open(ytids_path, 'r') as f:
@@ -23,6 +27,7 @@ class Dataset:
         self.class_associations = dict(class_associations)
         self.reverse_class_associations = dict((v,k) for k,v in class_associations)
         self.transform_fn = transform_fn
+        self.include_ytid = include_ytid
         
         if load_data:
             self.load_data()
@@ -42,8 +47,12 @@ class Dataset:
     def __getitem__(self, index):
         ytid = self.ytids[index]
         data_without_ytid = self.data[ytid]
-        data = {'ytid':ytid}
-        data.update(data_without_ytid)
+        if self.include_ytid:
+            data = {'ytid': ytid}
+            data.update(data_without_ytid)
+        else:
+            data = data_without_ytid
+        
         if self.transform_fn is not None:
             data = self.transform_fn(data)
         return data
@@ -54,10 +63,11 @@ class Dataset:
     
 
 class RandomWindow:
-    def __init__(self, window_size, features, pad_value):
+    def __init__(self, window_size, features, pad_value, include_window=False):
         self.window_size = window_size
         self.features = features
         self.pad_value = pad_value
+        self.include_window = include_window
         
     def __call__(self, data):
         data_out = data.copy()
@@ -70,13 +80,15 @@ class RandomWindow:
                 data_out[feature] = np.pad(data_out[feature], [[pad_size,0]]+[[0, 0]]*(len(data[feature].shape)-1),
                                         constant_values=self.pad_value)
             data_len = data_len + pad_size
-            data_out['window'] = np.array([0, data_len], dtype=np.int32)
+            if self.include_window:
+                data_out['window'] = np.array([0, data_len], dtype=np.int32)
         else:
             start_idx = np.random.randint(0, data_len-self.window_size+1, dtype=np.int32)
             end_idx = start_idx + self.window_size
             
             window = np.stack([start_idx, end_idx], axis=0)
-            data_out['window'] = window
+            if self.include_window:
+                data_out['window'] = window
             
             for feature in self.features:
                 data_out[feature] = data_out[feature][start_idx:end_idx]
